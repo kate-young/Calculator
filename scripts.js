d3691700af68850f68d7c6bc5eeeac79fea615f3
kate-young <s1391628@student.mcckc.edu>
2016-01-14 15:35:18 -0800

    add number as current value on click

--- scripts.js
+++ scripts.js
@@ -1,23 +1,38 @@
 var calculator = {
-    var current_value = 0,
-    
-    var add = function(value) {
+    current_value: 0,
+    add: function(value) {
         this.current_value += value;
     },
-
-    var multiply = function(value) {
+    multiply: function(value) {
         this.current_value += value;
     },
-    
-    var subtract = function(value) {
+    subtract: function(value) {
         this.current_value += value;
     },
-
-    var divide = function(value) {
+    divide: function(value) {
         this.current_value /= value;
     },
-
-    var clear = function(value) {
+    clear: function() {
         this.current_value = 0;
+    },
+    operate: function(value, operator) {
+        switch(operator) {
+        case "+": 
+            this.add(value);
+            break;
+        case "*":
+            this.multiply(value);
+            break;
+        case "/":
+            this.divide(value);
+            break;
+        default:
+            this.clear();
+        }
     }
 }
+$(document).ready(function() {
+    $(".number").on("click", function() {
+        calculator.current_value = parseInt($(this).text());
+    });
+});
